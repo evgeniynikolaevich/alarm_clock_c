ec0d919a5d83d084fdda54f9ba942e3691ddade3
evgeniy gazetdinov <zplacesound@gmail.com>
2019-06-17 16:00:02 +0300

    Add files via upload

---
+++ counter.c
@@ -0,0 +1,19 @@
+
+
+#include<stdio.h>
+#include <unistd.h>
+
+int main()
+
+
+{
+      for(int coun = 0;coun<100;coun ++){
+        sleep(1);
+        printf("%d\n",coun);
+    }
+
+
+
+        return 1;
+
+}
