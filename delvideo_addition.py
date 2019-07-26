1eb8dd1ded287ec8948789ef2e7385ec0467059e
evgeniy gazetdinov <zplacesound@gmail.com>
2019-07-26 07:49:12 +0300

    Add files via upload

--- delvideo_addition.py
+++ delvideo_addition.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 import re
-
+from collections import OrderedDict
 # from file_size import get_path_from_name,return_name,create_name_path
 
 # this for debug targets
@@ -12,7 +12,7 @@ _PATH = os.getcwd()
 
 
 def convert_name_to_datetime(name):
-      pattern = r'^\w{1,3}\d{1,3}\_\d\d_\d\d_\d{4}___\d\d_\d\d_\d\d'
+      pattern = r'\w{1,3}\d{1,3}_\d\d_\d\d_\d{4}___\d\d_\d\d_\d\d'
       # delete mp4 extens
       print('name for convert')
       print(name)
@@ -72,7 +72,7 @@ def find_all_files(path):
     # r=root, d=directories, f = files
     for r, d, f in os.walk(path):
         for file in f:
-            if re.match(r'^\w\w\w\w\d\d_\d\d_\d\d_\d{4}___\d\d_\d\d_\d\d.mp4', file):
+            if re.match(r'\w{1,3}\d{1,3}_\d\d_\d\d_\d{4}___\d\d_\d\d_\d\d.mp4', file):
                 files.append(os.path.join(r, file))
     return files
 
@@ -107,7 +107,7 @@ def delete_file(filename):
     os.remove(filename)
 
 def convert_to_date(name):
-    pattern = r'^\w{1,3}\d{1,3}\_\d\d_\d\d_\d{4}___\d\d_\d\d_\d\d'
+    pattern = r'\w{1,3}\d{1,3}_\d\d_\d\d_\d{4}___\d\d_\d\d_\d\d'
     # delete mp4 extens
     if re.match(pattern, name) is not None:
         if name is not None:
@@ -143,7 +143,7 @@ def create_pairs(files):
 
 
 def sort_pairs_by_date(pairs):
-    data_sorted = sorted(pairs, key=lambda item: item.keys())
+    data_sorted = sorted(pairs, key=lambda item: item.keys()[-1])
     return data_sorted
 
 
@@ -152,6 +152,7 @@ def delete_by_free_space(limit, free_space=0):
         videos = search_videos()
         pairs = map(create_pairs, videos)
         sorted_pairs = sort_pairs_by_date(pairs)
+        print(sorted_pairs)
         delete_handler(sorted_pairs,limit,free_space)
 
 
@@ -168,44 +169,5 @@ def return_name(path):
 
 def create_name_path(name, path):
   # name-key is time of creation
-  name_path = {name: path}
+  name_path = OrderedDict({name: path})
   return name_path
-
-
-def convert_name_to_datetime(name):
-  pattern = r'^\w{1,3}\d{1,3}\_\d\d_\d\d_\d{4}___\d\d_\d\d_\d\d'
-  # delete mp4 extens
-  if re.match(pattern, name) is not None:
-      if name is not None:
-          w_ext = name.split('_')
-          # CONVERT!!!!
-          # time_creation = datetime.time(year =int(w_ext[3]),month=int(w_ext[2]),day =int(w_ext[1]),
-          #                              hour = int(w_ext[6]),min = int(w_ext[7]),sec = int(month[8])
-          # fix datetime
-          dt = datetime.datetime(int(w_ext[3]), int(w_ext[2]), int(w_ext[1]))
-          tm = datetime.time(int(w_ext[6]), int(w_ext[7]), int(w_ext[8]))
-          time_creation = dt.combine(dt, tm)
-          # datetime_object = datetime.strptime('{}/{}/{} {}:{}:{}'.format(int(w_ext[1]),int(w_ext[2]),int(w_ext[3]), int(w_ext[6]), int(w_ext[7]), int(w_ext[8])), '%m/%d/%Y %I:%M%p')
-          print(time_creation)
-          return time_creation
-
-
-  def create_limit():
-      today = datetime.datetime.now()
-      hour = datetime.timedelta(hours=1)
-      limit = today - hour
-      return limit
-
-
-  def create_name_path(name, path):
-      # name-key is time of creation
-      name_path = {name: path}
-      return name_path
-
-
-
-  def find_weight(path_to_file):
-      if path_to_file is not None:
-          statinfo = os.stat(path_to_file)
-          size = statinfo.st_size
-          return size
